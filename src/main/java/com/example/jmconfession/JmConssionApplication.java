8ee62082d6d7d1f6a50a376a79dfe8e4a4e4d9a6
IdonNeedNAME <1992140645@qq.com>
2025-09-17 12:41:24 +0800

    dddd

---
+++ src/main/java/com/example/jmconfession/JmConssionApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmconfession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmConssionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmConssionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jmconfession/JmConssionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmconfession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmConssionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
